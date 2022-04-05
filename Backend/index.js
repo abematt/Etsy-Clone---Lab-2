d8f9d6b8a1570c9197f1a1acccc49dc74c8d744f
Abraham Mathew <abrahammathew.dev@gmail.com>
2022-04-04 23:31:55 -0700

    Added model for user model and routes for basic insert into collection

--- Backend/index.js
+++ Backend/index.js
@@ -11,9 +11,12 @@ const mongooseConnection = require('./config/database')
 
 //API Routes for CRUD operations
 const testRoute = require('./routes/test')
+const userRoute = require('./routes/user')
 
 //Defining api endpoints
+app.use(express.json())
 app.use("/api/test",testRoute)
+app.use("/api/auth",userRoute)
 
 app.listen(3001);
 console.log("Server listening on 3001");
