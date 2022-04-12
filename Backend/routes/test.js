1323c122ef2272eeaff85370d1b781bc40444436
Abraham Mathew <abrahammathew.dev@gmail.com>
2022-04-11 19:15:10 -0700

    Integrated Kafka for making test request

--- Backend/routes/test.js
+++ Backend/routes/test.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
+var kafka = require('../kafka/client')
 
 
 router.get("/testAPI", (req,res)=> {
     res.send("this test is successful")
 });
 
+router.post("/testAPI2", (req,res)=> {
+    kafka.make_request('post_book',req.body, function(err,results){
+        if (err){
+            console.log("Inside err");
+            res.json({
+                status:"error",
+                msg:"System Error, Try Again.",
+                error:err
+            })
+        }else{
+            console.log("Inside else");
+                res.json("successfully completed")
+
+                res.end();
+            }
+    })
+    
+})
 module.exports = router
